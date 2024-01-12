55c4ba9945845bb2e56c53793cda561119ddf257
JuliaLavagnini <>
2024-01-12 23:11:15 +0000

    Fixing connectio between Flask and MongoDB

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from pymongo import MongoClient
 if os.path.exists("env.py"):
     import env
 
@@ -23,7 +24,21 @@ def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("my-tasks.html", tasks=tasks)
 
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+
+uri = "mongodb+srv://julialavagninimaia:nOMlYqDw7ubfR9sX@cluster0.iprbu29.mongodb.net/task_manager?retryWrites=true&w=majority"
+
+try:
+    # Attempt to connect to MongoDB
+    client = MongoClient(uri)
+    db = client.get_database()
+    print("Connected to MongoDB successfully!")
+
+    # Perform MongoDB operations here
+
+except Exception as e:
+    print(f"Error connecting to MongoDB: {e}")
