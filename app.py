436e0456fa4e5598cd294e0e53d2044b3837b75c
JuliaLavagnini <>
2024-01-13 00:40:37 +0000

    Add sign in content

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from pymongo import MongoClient
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -23,6 +24,11 @@ mongo = PyMongo(app)
 def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("my-tasks.html", tasks=tasks)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("sign-in.html")
 
 
 if __name__ == "__main__":
