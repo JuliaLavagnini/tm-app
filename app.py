3e087086c8ba3214dc8564fc1afda931ab32c996
Julia Lavagnini Maia <65735429+JuliaLavagnini@users.noreply.github.com>
2024-09-24 17:59:54 +0100

    Trying to see debug issue

--- app.py
+++ app.py
@@ -190,4 +190,4 @@ def mark_done(task_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)  # Run the Flask app
+            debug=True)  # Run the Flask app
